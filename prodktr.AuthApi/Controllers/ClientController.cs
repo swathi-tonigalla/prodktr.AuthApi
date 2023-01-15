9682e7b8587828555b8b5b17d05a591494c8755b
swathi-tonigalla <swathi.tonigandla@gmail.com>
2023-01-15 19:29:18 +0530

    Added controllers

---
+++ prodktr.AuthApi/Controllers/ClientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prodktr.AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+    }
+}
---
+++ prodktr.AuthApi/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prodktr.AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
